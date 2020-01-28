88d7f3224cafafd5a0877abc40c21e47290beaa0
lesliesaucedo17 <you@example.com>
2020-01-27 21:35:50 -0600

    version inicial

---
+++ hola mundo- practica 1.c
@@ -0,0 +1,6 @@
+#include "stdio.h"
+int main ()
+{
+	printf ("Hola mundo 1930124.\n");
+	return 0;
+	}
---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (int argc, char *argv[])
+{
+	printf ("Hola mundo 1930124.\n");
+	return 0;
+}
