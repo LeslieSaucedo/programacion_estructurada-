d830667328b5f5f6ec1422fd8a0699449ab0287b
lesliesaucedo17 <51415403+lesliesaucedo17@users.noreply.github.com>
2020-03-02 20:57:41 -0600

    Add files via upload

---
+++ practica_6.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+	int num=0,sum=0,cant=0;
+	float prom=0;
+	do{
+		printf("Introduzca un numero entero\nPara terminar ingrese -1\n\t");
+		sum+=num;
+		cant++;
+		scanf("%d",&num);	
+	}while(num>=0);
+	printf("La suma de los numeros ingresados es:%d\nLa cantidad de numeros es %d\nEl promedio de los numeros es: %.2f\n\n",sum,cant-1,prom);
+	system("pause");
+	return 0;
+}
