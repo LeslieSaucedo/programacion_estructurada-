c33e3f9ee39448a7d0f24aaf35d1842607b612f5
Leslie Saucedo <51415403+lesliesaucedo17@users.noreply.github.com>
2020-06-05 14:58:46 -0500

    Update practica_10.c

--- practica_10.c
+++ practica_10.c
@@ -1,67 +1,213 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdbool.h>
 #include <string.h>
+#include <stdlib.h>
 
+#define POSIBLES 2
 
-struct informacion {
-	char ID[10];
-	char nombre[15];
-	char pApellido[15];
-	char sApellido[15];
-	char bDate[10];
-	char nEscuela[40];
-	char grado[10];
-	char career[15];
-	char genero[12];
-}estudiante;
+struct student
+{
+	int studentId;
+	char firstName[' '];
+	char firstLastName[' '];
+	char secondLastName[' '];
+	char birthDate[' '];
+	char school[' '];
+	char grade[' '];
+	char career[' '];
+	char gender[' '];
+};
 
-int main(){
-	printf("Ingrese el ID del alumno: ");
-	gets(estudiante.ID);
-	//strcpy(informacion[0], aux);
-	system("cls");
-	
-	printf("Ingrese el nombre del alumno: ");
-	gets(estudiante.nombre);
-	system("cls");
-	
-	printf("Ingrese el primer apellido del alumno: ");
-	gets(estudiante.pApellido);
-	system("cls");
-	
-	printf("Ingrese el segundo apellido del alumno: ");
-	gets(estudiante.sApellido);
-	system("cls");
-	
-	printf("Ingrese la fecha de nacimiento del alumno: ");
-	gets(estudiante.bDate);
-	system("cls");
-	
-	printf("Ingrese el nombre de la escuela del alumno: ");
-	gets(estudiante.nEscuela);
-	system("cls");
-	
-	printf("Ingrese el grado del alumno: ");
-	gets(estudiante.grado);
-	system("cls");
-	
-	printf("Ingrese la carrera del alumno: ");
-	gets(estudiante.career);
-	system("cls");
-	
-	printf("Ingrese el genero del alumno: ");
-	gets(estudiante.genero);
-	system("cls");
+int main()
+{
+	struct student studentList[POSIBLES];
+	int opcion, n = 0, i, j;
 
-	printf("El ID del alumno es: %s\n\n", estudiante.ID);
-	printf("El nombre del alumno es: %s\n\n", estudiante.nombre);
-	printf("El apellido del alumno es: %s\n\n", estudiante.pApellido);
-	printf("El segundo apellido del alumno es: %s\n\n", estudiante.sApellido);
-	printf("la fecha de nacimiento del alumno es: %s\n\n", estudiante.bDate);
-	printf("El nombre la escuela del alumno es: %s\n\n", estudiante.nEscuela);
-	printf("El grado del alumno es: %s\n\n", estudiante.grado);
-	printf("La carrera del alumno es: %s\n\n", estudiante.career);
-	printf("El genero del alumno es: %s\n\n", estudiante.genero);
+	// variables temporales
+	bool valido;
+	int temp;
+	char temporal[' '];
+	
+	// Control de registro de estudiantes
+	for (i = 0; i < POSIBLES; i++)
+	{
+		printf("\t\t\t\t\t\t\t-REPORTE-\n\n");
+		
+		// Matricula
+		do
+		{
+			printf("Introduzca la matricula del estudiante %i: ", n+1);
+			scanf("%i", &temp);
+			
+			valido = true;
+			
+			if (temp < 1)
+			{
+				valido = false;
+				printf("\n\tError: la matricula debe ser mayor a 0\n\n");
+			}
+			else
+				for (j = 0; j < n; j++)
+					if (temp == studentList[j].studentId)
+					{
+						valido = false;
+						printf("\n\tError: la matricula introducida ya le pertenece a otro estudiante\n\n");
+					}
+		}
+		while (!valido);
+		studentList[n].studentId = temp;
+		
+		// Primer nombre
+		do
+		{
+			printf("Introduzca el primer nombre(s) del estudiante %i: ", i+1);
+			fflush(stdin);
+			gets(temporal);
+			
+			if (strlen(temporal) < 1)
+				printf("\n\tError: introduzca un nombre\n\n");
+			else
+				if (strlen(temporal) > 32)
+					printf("\n\tError: el limite de caracteres es de 32\n\n");
+		}
+		while (strlen(temporal) < 1 || strlen(temporal) > 32);
+		strcpy(studentList[n].firstName, temporal);
+		
+		// Primer apellido
+		do
+		{
+			printf("Introduzca el primer apellido del estudiante %i: ", i+1);
+			fflush(stdin);
+			gets(temporal);
+			
+			if (strlen(temporal) < 1)
+				printf("\n\tError: introduzca el primer apellido\n\n");
+			else
+				if (strlen(temporal) > 100)
+					printf("\n\tError: el limite de caracteres es de 32\n\n");
+		}
+		while (strlen(temporal) < 1 || strlen(temporal) > 32);
+		strcpy(studentList[n].firstLastName, temporal);
+		
+		// Segundo apellido
+		do
+		{
+			printf("Introduzca el segundo apellido del estudiante %i (opcional): ", i+1);
+			fflush(stdin);
+			gets(temporal);
+			
+			if (strlen(temporal) > 32)
+				printf("\n\tError: el limite de caracteres es de 32\n\n");
+		}
+		while (strlen(temporal) > 32);
+		strcpy(studentList[n].secondLastName, temporal);
+		
+		// Fecha de nacimiento
+		do
+		{
+			printf("Introduzca la fecha de nacimiento del estudiante %i usando el formato dd/mm/yyyy, incluyendo los '/': ", i+1);
+			fflush(stdin);
+			gets(temporal);
+			
+			if (strlen(temporal) < 1)
+				printf("\n\tError: introduzca la fecha de nacimiento\n\n");
+			else
+				if (strlen(temporal) > 10)
+					printf("\n\tError: el limite de caracteres es de 10\n\n");
+				else
+					if (temporal[2] != '/' || temporal[5] != '/' || strlen(temporal) != 10)
+						printf("\n\tError: la fecha de nacimiento es invalida\n\n");
+		}
+		while (strlen(temporal) < 1 || strlen(temporal) > 10 || temporal[2] != '/' || temporal[5] != '/' || strlen(temporal) != 10);
+		strcpy(studentList[n].birthDate, temporal);
+		
+		// Escuela
+		do
+		{
+			printf("Introduzca el nombre de la escuela del estudiante %i: ", i+1);
+			fflush(stdin);
+			gets(temporal);
+			
+			if (strlen(temporal) < 1)
+				printf("\n\tError: introduzca el nombre de la escuela\n\n");
+			else
+				if (strlen(temporal) > 32)
+					printf("\n\tError: el limite de caracteres es de 32\n\n");
+		}
+		while (strlen(temporal) < 1 || strlen(temporal) > 32);
+		strcpy(studentList[n].school, temporal);
+		
+		// Grado
+		do
+		{
+			printf("Introduzca el grado del estudiante %i: ", i+1);
+			fflush(stdin);
+			gets(temporal);
+			
+			if (strlen(temporal) < 1)
+				printf("\n\tError: introduzca el grado\n\n");
+			else
+				if (strlen(temporal) > 2)
+					printf("\n\tError: el limite de caracteres es de 2\n\n");
+		}
+		while (strlen(temporal) < 1 || strlen(temporal) > 2);
+		strcpy(studentList[n].grade, temporal);
+		
+		// Carrera
+		do
+		{
+			printf("Introduzca la carrera del estudiante %i: ", i+1);
+			fflush(stdin);
+			gets(temporal);
+			
+			if (strlen(temporal) < 1)
+				printf("\n\tError: introduzca la carrera\n\n");
+			else
+				if (strlen(temporal) > 32)
+					printf("\n\tError: el limite de caracteres es de 32\n\n");
+		}
+		while (strlen(temporal) < 1 || strlen(temporal) > 32);
+		strcpy(studentList[n].career, temporal);
+		
+		// Genero
+		do
+		{
+			printf("Introduzca el genero del estudiante %i: ", i+1);
+			fflush(stdin);
+			gets(temporal);
+			
+			if (strlen(temporal) < 1)
+				printf("\n\tError: introduzca el genero\n\n");
+			else
+				if (strlen(temporal) > 32)
+					printf("\n\tError: el limite de caracteres es de 32\n\n");
+		}
+		while (strlen(temporal) < 1 || strlen(temporal) > 32);
+		strcpy(studentList[n].gender, temporal);
+		
+		n++;
+		
+		system("cls");
+	}
+	
+	printf(""\t\t\t\t\t\t\t-REPORTE-\n\n"");
+	for (i = 0; i < n; i++)
+	{
+		printf("Estudiante %i\n", i+1);
+		printf("\tMatricula: %i\n", studentList[i].studentId);
+		printf("\tPrimer nombre: %s\n", studentList[i].firstName);
+		printf("\tPrimer apellido: %s\n", studentList[i].firstLastName);
+		if (strlen(studentList[i].secondLastName) > 0)
+			printf("\tSegundo apellido: %s\n", studentList[i].secondLastName);
+		printf("\tFecha de nacimiento: %s\n", studentList[i].birthDate);
+		printf("\tEscuela: %s\n", studentList[i].school);
+		printf("\tGrado: %s\n", studentList[i].grade);
+		printf("\tCarrera: %s\n", studentList[i].career);
+		printf("\tGenero: %s\n", studentList[i].gender);
+		printf("\n\n");
+	}
+	
+	system("pause");
+	return 0;
 }
-
 
